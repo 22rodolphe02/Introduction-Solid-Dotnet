b2d991407cbe31c08aa611978a79bcff6230e988
22rodolphe02 <tsiafararodolphe@gmail.com>
2024-01-09 19:01:35 +0300

    update project

--- Organic/obj/Debug/net7.0/Organic.AssemblyInfo.cs
+++ Organic/obj/Debug/net7.0/Organic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Organic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ab4e29a909afd10a7cf921fa15daa8feb4ceb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Organic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Organic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
